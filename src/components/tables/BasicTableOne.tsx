4be8e7568d201f510bbc2dd8917c8a2293b32414
elvinyekayev <elvinyekay@gmail.com>
2025-10-25 20:20:57 +0400

    placeholder image

--- src/components/tables/BasicTableOne.tsx
+++ src/components/tables/BasicTableOne.tsx
@@ -142,7 +142,7 @@ export default function BasicTableOne ({citizens, setCitizens}: TableProps) {
                                                 <Image
                                                     width={40}
                                                     height={40}
-                                                    src={"/images/user.jpg"}
+                                                    src={"/images/user.png"}
                                                     alt={citizen.name}
                                                 />
                                             </div>
